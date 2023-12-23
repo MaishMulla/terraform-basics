5adbf70864eb87b1fe50db688058873da83e514d
maishmulla <mulla.maish@gmail.com>
2023-12-22 20:45:30 -0500

    new shell

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -23,5 +23,15 @@ variable "example_list"{
     value = "Welcome To ${var.example_list[0]} with ${var.example_list[1]} Training and this our batch ${var.example_list[4]} and training by ${var.example_list[3]}"
 }
 
+variable "training_map" {
+    default = {
+        batch       = "b56" 
+        mode        = "Online"
+        training    = "DevOpsWithAWS"
+    }
+}
 
+output "training_map_op" {
+    value = "${var.training_map["training"]} has great scope of opportunities and this is complete ${var.training_map["mode"]} and this is our batch ${var.training_map["batch"]}"
+}
 
