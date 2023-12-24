9e0bb742de5cbff20271c3947b1c14228ab86913
maishmulla <mulla.maish@gmail.com>
2023-12-23 23:11:24 -0500

    terraform

--- 03-ec2-instance/ec.tf
+++ 03-ec2-instance/ec.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "app" {
   ami                    = "ami-06911ef5687b5d1ad"                                     # argument
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-01210c7d60b16e137]
+ # vpc_security_group_ids = [sg-01210c7d60b16e137]
 
     tags = {
-             Name        = "DevOps-With-AWS-LabInstance"
+             Name        = "Helloworld"
     }
 }
